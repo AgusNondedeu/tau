dea229cca756130bc8da4e0bf53afefc4dbd9313
andy <arteysoft@gmail.com>
2022-01-03 12:55:58 -0300

    agrego ahora servicio saludo

---
+++ clase1/tau/src/main/java/edu/it/ServicioSaludo.java
@@ -0,0 +1,9 @@
+package edu.it;
+
+import javax.mispec.menu.MenuItem;
+
+public class ServicioSaludo implements MenuItem {
+	public void ejecutarOpcionMenu() {
+		System.out.println("Ejecutando el modulo servicio saludo...");
+	}
+}
